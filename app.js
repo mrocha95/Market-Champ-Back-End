11a89bb501b1f008820e258c68e85b9d6f4282ae
Miguel Rocha <mrocha1095@gmail.com>
2022-09-22 22:18:16 -0400

    updating2

--- app.js
+++ app.js
@@ -49,18 +49,18 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-mongoose
-  .connect("mongodb://localhost/market-champ")
-  .then((x) =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch((err) => console.error("Error connecting to mongo", err));
-
 // mongoose
-//   .connect(process.env.MONGODB_URI)
+//   .connect("mongodb://localhost/market-champ")
 //   .then((x) =>
 //     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
 //   )
 //   .catch((err) => console.error("Error connecting to mongo", err));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 module.exports = app;
